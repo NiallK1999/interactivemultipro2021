c3a8e3abd2123f03d4dda1029823b8295c4813c9
NiallK1999 <64143241+NiallK1999@users.noreply.github.com>
2021-11-26 22:52:07 +0000

    Add files via upload

---
+++ Scripts2/KeyScripts.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "player")
+        {
+            GameVariables.keyCount += 2;
+            Destroy(gameObject);
+        }
+    }
+    
+        
+    }
+
